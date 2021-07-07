0f1c2261122226b84aa90b72715e508faa9113d1
dahaekang <86761427+dahaekang@users.noreply.github.com>
2021-07-07 16:34:32 +0900

    history upload

--- gulpfile.js
+++ gulpfile.js
@@ -240,14 +240,7 @@ exports.etc = etc;
 exports.testPathServer = testPathServer;
 exports.testPathLocal = testPathLocal;
 exports.default = parallel(bSync, watching);
-<<<<<<< HEAD
-exports.serve = parallel(series(parallel(template), css, js, img, etc, bSync), watching);
-exports.build = parallel(series(parallel(template), css, js, img, etc, bSync), watching);
 
-exports.land = parallel(series(parallel(template), css, js, img, etc, bSyncLanding), watching);
-
-exports.test = series(parallel(template), css, js, img, etc, copyTest, testPathServer, bSyncTest);
-=======
 exports.serve = parallel(
   series(parallel(template), css, js, img, etc, bSync),
   watching
@@ -272,4 +265,4 @@ exports.test = series(
   testPathServer,
   bSyncTest
 );
->>>>>>> 08b3023357105881ebe8e4ca8fb33607bf77e429
+
